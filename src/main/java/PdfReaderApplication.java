3b7bc9a92422d205e5351fc7c7c9b2070238d8a5
Anthony M <am13571@my.bristol.ac.uk>
2019-06-09 16:57:31 +0100

    Converts to buffered and reads pixels

--- src/main/java/PdfReaderApplication.java
+++ src/main/java/PdfReaderApplication.java
@@ -3,6 +3,7 @@ import org.ghost4j.renderer.SimpleRenderer;
 
 import javax.imageio.ImageIO;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.util.List;
@@ -11,25 +12,25 @@ public class PdfReaderApplication {
 
     public static void main(String[] args) {
 
-        PDFDocument pdf = new PDFDocument();
+        final PDFDocument pdfDoc = new PDFDocument();
+        final File pdfFile = new File("pdfs/gameRules.pdf");
         try {
-            pdf.load(new File("dummy.pdf"));
+            pdfDoc.load(pdfFile);
 
             final SimpleRenderer renderer = new SimpleRenderer();
-            renderer.setResolution(300);
+            renderer.setResolution(30);
 
-            final List<Image> images = renderer.render(pdf);
+            final Image image = renderer.render(pdfDoc, 0, 0).get(0);
 
-            final Image image = images.get(0);
+            final File file = new File("pngs/gameRules.png");
 
-            ImageIO.write((RenderedImage) image, "png", new File("dummy.png"));
+            ImageIO.write((RenderedImage) image, "png", file);
 
-            final int x = 1;
-
-
-
-
-
+            final BufferedImage bufferedImage = ImageIO.read(file);
+            final int rgb = bufferedImage.getRGB(0, 0);
+            int  red   = (rgb & 0x00ff0000) >> 16;
+            int  green = (rgb & 0x0000ff00) >> 8;
+            int  blue  =  rgb & 0x000000ff;
 
         } catch (Exception e) {
             System.out.println(e);
